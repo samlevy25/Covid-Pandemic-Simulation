bed9baf316377d4c83dd4f061e6dd8a821e17a0d
levysamuel <samuel152@hotmail.fr>
2021-04-29 14:27:14 +0300

    edit StatisticsFile

--- src/IO/StatisticsFile.java
+++ src/IO/StatisticsFile.java
@@ -1,8 +1,35 @@
 package IO;
 
+import java.io.*;
+
 public class StatisticsFile {
     private String nameOfFile;
+
     StatisticsFile(String n) {
         nameOfFile = n;
+    }
+
+    public void writeInCvs( String[][] saveData) throws IOException {
+        try {
+            BufferedWriter br = new BufferedWriter(new FileWriter(this.nameOfFile));
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0 ; i < saveData.length ; i++)
+            {
+                for (int j = 0; j < saveData[i].length ; j++)
+                {
+                    sb.append(saveData[i][j]);
+                   if ( saveData[i][j] != saveData[i][saveData[i].length - 1])
+                       sb.append(";");
+
+                }
+
+            }
+
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
